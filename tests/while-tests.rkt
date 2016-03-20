1412756d4da70c80c672510a79b6331b95826727
Alan Borning <borning@cs.washington.edu>
2016-03-20 08:52:16 +0100

    cleaned up while tests to always use interesting-time correctly

--- tests/while-tests.rkt
+++ tests/while-tests.rkt
@@ -8,85 +8,6 @@
 
 (provide while-tests)
 
-(define (one-while-no-interesting)
-  (test-case
-   "test advance time with one while and no interesting times identified"
-   (define tester%
-     (class reactive-thing%
-       (inherit milliseconds)
-       (super-new)
-       ; To detect times the while fires, we have a variable x that is asserted to be equal to (milliseconds)
-       ; in the body of the while.  The stay causes it to remain at that value afterward.
-       (define-symbolic* x real?)
-       (assert (equal? x 5))
-       (send this solve)
-       (stay x)
-       (while (and (>= (milliseconds) 50) (<= (milliseconds) 100))
-              (assert (equal? x (milliseconds))))
-       (define/public (get-x) (send this wally-evaluate x))))
-   (define r (new tester%))
-   (send-thing r advance-time 10)
-   (check equal? (send-syncd r milliseconds-syncd) 10)
-   (check equal? (send r get-x) 5)  ; x has its original value
-   (send-thing r advance-time 60)
-   (check equal? (send-syncd r milliseconds-syncd) 60)
-   (check equal? (send r get-x) 60)
-   (send-thing r advance-time 75)
-   (check equal? (send-syncd r milliseconds-syncd) 75)
-   (check equal? (send r get-x) 75)
-   (send-thing r advance-time 200)
-   (check equal? (send-syncd r milliseconds-syncd) 200)
-   (check equal? (send r get-x) 75)))  ; x stays at 75 since we don't fire the while at 200
-   
-(define (one-while-no-interesting-hop-over)
-  (test-case
-   "similar to one-while-no-interesting but hop over the interval in which the while is true"
-   (define tester%
-     (class reactive-thing%
-       (inherit milliseconds)
-       (super-new)
-       (define-symbolic* x real?)
-       (assert (equal? x 5))
-       (send this solve)
-       (stay x)
-       (while (and (>= (milliseconds) 50) (<= (milliseconds) 100))
-              (assert (equal? x (milliseconds))))
-       (define/public (get-x) (send this wally-evaluate x))))
-   (define r (new tester%))
-   (send-thing r advance-time 10)
-   (check equal? (send-syncd r milliseconds-syncd) 10)
-   (check equal? (send r get-x) 5)
-   (send-thing r advance-time 150)
-   (check equal? (send-syncd r milliseconds-syncd) 150)
-   (check equal? (send r get-x) 5)))
-   
-(define (one-while-soft)
-  (test-case
-   "test advance time with one while - detect the effect using a soft constraint that can be satisfied when the 'while' is off"
-   (define tester%
-     (class reactive-thing%
-       (inherit milliseconds)
-       (super-new)
-       (define-symbolic* x integer?)
-       (always (equal? x 1) #:priority low)
-       (while (and (>= (milliseconds) 50) (<= (milliseconds) 100))
-              #:interesting-time (or (equal? (milliseconds) 50) (equal? (milliseconds) 100))
-              (assert (equal? x 2)))
-       (define/public (get-x) (send this wally-evaluate x))))
-   (define r (new tester%))
-   (send-thing r advance-time 10)
-   (check equal? (send-syncd r milliseconds-syncd) 10)
-   (check equal? (send r get-x) 1)
-   (send-thing r advance-time 60)
-   (check equal? (send-syncd r milliseconds-syncd) 60)
-   (check equal? (send r get-x) 2)
-   (send-thing r advance-time 75)
-   (check equal? (send-syncd r milliseconds-syncd) 75)
-   (check equal? (send r get-x) 2)
-   (send-thing r advance-time 200)
-   (check equal? (send-syncd r milliseconds-syncd) 200)
-   (check equal? (send r get-x) 1)))
-   
 (define (one-while-interesting)
   (test-case
    "test advance time with one while and the start and end of the while identified as interesting times"
@@ -118,7 +39,7 @@
 
 (define (one-while-interesting-hop-over)
   (test-case
-   "test advance time with one while and the start and end of the while identified as interesting times, hopping over the whole interval"
+   "same as one-while-interesting, but hop over the whole interval"
    (define tester%
      (class reactive-thing%
        (inherit milliseconds)
@@ -139,14 +60,39 @@
    (check equal? (send-syncd r milliseconds-syncd) 150)
    (check equal? (send r get-x) 100)))
 
+(define (one-while-soft)
+  (test-case
+   "test advance time with one while - detect the effect using a soft constraint that can be satisfied when the 'while' is off"
+   (define tester%
+     (class reactive-thing%
+       (inherit milliseconds)
+       (super-new)
+       (define-symbolic* x integer?)
+       (always (equal? x 1) #:priority low)
+       (while (and (>= (milliseconds) 50) (<= (milliseconds) 100))
+              #:interesting-time (or (equal? (milliseconds) 50) (equal? (milliseconds) 100))
+              (assert (equal? x 2)))
+       (define/public (get-x) (send this wally-evaluate x))))
+   (define r (new tester%))
+   (send-thing r advance-time 10)
+   (check equal? (send-syncd r milliseconds-syncd) 10)
+   (check equal? (send r get-x) 1)
+   (send-thing r advance-time 60)
+   (check equal? (send-syncd r milliseconds-syncd) 60)
+   (check equal? (send r get-x) 2)
+   (send-thing r advance-time 75)
+   (check equal? (send-syncd r milliseconds-syncd) 75)
+   (check equal? (send r get-x) 2)
+   (send-thing r advance-time 200)
+   (check equal? (send-syncd r milliseconds-syncd) 200)
+   (check equal? (send r get-x) 1)))
+
 (define while-tests 
   (test-suite+
    "run tests for while in reactive-thing"
-   (one-while-no-interesting)
-   (one-while-no-interesting-hop-over)
-   (one-while-soft)
    (one-while-interesting)
    (one-while-interesting-hop-over)
+   (one-while-soft)
    ))
 
 (time (run-tests while-tests))
