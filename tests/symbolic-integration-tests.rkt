d6145f96ee42a37d407f372491f31312ede2293f
Alan Borning <borning@cs.washington.edu>
2016-08-19 18:50:51 +0200

    added support for double (and triple) integration

--- tests/symbolic-integration-tests.rkt
+++ tests/symbolic-integration-tests.rkt
@@ -13,31 +13,44 @@
   (test-case
    "simple cases for symbolic integration -- expression is atomic"
    (check equal? (symbolic-integral 2 'x) '(* 2 x))
-   (check equal? (symbolic-integral 'x 'x) '(* 0.5 x x))
+   (check equal? (symbolic-integral 'x 'x) '(* 0.5 (expt x 2)))
    ; case of (milliseconds) -- variable of integration is a function call (kind of funky - maybe fix this later)
    (check equal? (symbolic-integral 2 '(milliseconds)) '(* 2 (milliseconds)))
-   (check equal? (symbolic-integral '(milliseconds) '(milliseconds)) '(* 0.5 (milliseconds) (milliseconds)))))
+   (check equal? (symbolic-integral '(milliseconds) '(milliseconds)) '(* 0.5 (expt (milliseconds) 2)))))
 
 (define (compound-expression-tests)
   (test-case
    "integral of more complex expressions"
-   (check equal? (symbolic-integral '(+ x 3) 'x) '(+ (* 0.5 x x) (* 3 x)))
-   (check equal? (symbolic-integral '(+ 3 x) 'x) '(+ (* 3 x) (* 0.5 x x)))
-   (check equal? (symbolic-integral '(* x 3) 'x) '(* 3 (* 0.5 x x) ))
-   (check equal? (symbolic-integral '(* 3 x) 'x) '(* 3 (* 0.5 x x) ))
+   (check equal? (symbolic-integral '(+ x 3) 'x) '(+ (* 0.5 (expt x 2)) (* 3 x)))
+   (check equal? (symbolic-integral '(+ 3 x) 'x) '(+ (* 3 x) (* 0.5 (expt x 2))))
+   (check equal? (symbolic-integral '(* x 3) 'x) '(* 3 (* 0.5 (expt x 2))))
+   (check equal? (symbolic-integral '(* 3 x) 'x) '(* 3 (* 0.5 (expt x 2))))
+   (check equal? (symbolic-integral '(/ x 3) 'x) '(/ (* 0.5 (expt x 2)) 3))
    (check equal? (symbolic-integral '(expt x 3)'x) '(/ (expt x 4) 4))
+   ))
+
+(define (double-triple-integral-tests)
+  (test-case
+   "double and triple integrals"
+   (check equal? (symbolic-integral '(integral 10) 'x) '(* 10 (* 0.5 (expt x 2))))
+   (check equal? (symbolic-integral '(integral x) 'x) '(* 0.5 (/ (expt x 3) 3)))
+   (check equal? (symbolic-integral '(integral (integral 10)) 'x) '(* 10 (* 0.5 (/ (expt x 3) 3))))
    ))
 
 (define (too-difficult-expression-test)
   (test-case
-   "an expression that is too difficult"
-   (check-exn exn:fail? (lambda () (symbolic-integral '(sin x) 'x)))))
+   "expressions that are too difficult"
+   (check-exn exn:fail? (lambda () (symbolic-integral '(sin x) 'x)))
+   (check-exn exn:fail? (lambda () (symbolic-integral '(* x x) 'x)))  ; needs to use expt
+   (check-exn exn:fail? (lambda () (symbolic-integral '(/ 2 x) 'x)))
+   ))
 
 (define symbolic-integration-tests 
   (test-suite+
    "run tests for symbolic integration function"
    (atomic-expression-tests)
    (compound-expression-tests)
+   (double-triple-integral-tests)
    (too-difficult-expression-test)
    ))
 
